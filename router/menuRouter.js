b867cb4128034ac1c6c95151a5b620deb564757c
sangyunpark99 <psangyun5108@gmail.com>
2023-04-11 17:06:12 +0900

    master 브랜치 code 가져오기

--- router/menuRouter.js
+++ router/menuRouter.js
@@ -1,38 +1,24 @@
 const express = require("express");
 const router = express.Router();
-const fs = require("fs");
-const path = require("path");
-const env = require("dotenv");
-
-env.config();
-
-const getImage = require("../utils/getImage");
 
 // GET /menu 요청에 대한 응답 함수
-router.get("/", (req, res) => {
-  // 이미지 이름 저장에 필요한 항목 - 년, 월, 일
-  const date = new Date();
-  const year = date.getFullYear();
-  const month = date.getMonth() + 1;
-  const day = date.getDate();
-
+router.post("/", (req, res) => {
   try {
-    const imageName = `menu_${`${year}-${month
-      .toString()
-      .padStart(2, "0")}-${day.toString().padStart(2, "0")}`}.png`;
+    const imageName = "menu.png";
 
-    // 절대 경로로 지정 -> 파일 경로 오류 방지
-    const filePath = path.join(__dirname, `../public/image/${imageName}`);
-
-    if (!fs.existsSync(filePath)) {
-      getImage();
-    }
-
-    res
-      .status(200)
-      .json({
-        imageUrl: `http://${process.env.ADDRESS}:${process.env.PORT}/${imageName}`,
-      });
+    res.status(200).json({
+      version: "2.0",
+      template: {
+        outputs: [
+          {
+            simpleImage: {
+              imageUrl: `http://sangyun5108.cafe24app.com/${imageName}`,
+              altText: "학식입니다.",
+            },
+          },
+        ],
+      },
+    });
   } catch (e) {
     console.error(e.message);
     res.status(500).send("Server Error.");
--- web.js
+++ web.js
@@ -6,14 +6,7 @@ const menuRouter = require("./router/menuRouter");
 const schedule = require("node-schedule");
 const morgan = require("morgan");
 
-const folderName = "./public/image";
 const app = express();
-
-if (!fs.existsSync(folderName)) {
-  // 폴더가 존재하지 않는 경우
-  fs.mkdirSync(folderName);
-  console.log(`${folderName} 생성!`);
-}
 
 const job = schedule.scheduleJob("0 15 * * *", () => {
   getImage();
@@ -26,22 +19,6 @@ app.use(express.static(imagePath));
 app.use(morgan("dev"));
 
 app.use("/menu", menuRouter);
-app.use("/", (req, res) => {
-  const resJson = {
-    version: "2.0",
-    template: {
-      outputs: [
-        {
-          simpleText: {
-            text: "간단한 텍스트 요소입니다.",
-          },
-        },
-      ],
-    },
-  };
-
-  res.json(resJson);
-});
 
 app.listen("8001", () => {
   console.log("서버 실행중..!");
